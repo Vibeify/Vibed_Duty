8c3e72fbfdc1580573a955303b234f78917b7479
Vibeify <vibeifymedia@gmail.com>
2025-05-20 20:46:10 -0400

    moved afk to config

--- config.lua
+++ config.lua
@@ -2,6 +2,8 @@ Config = {}
 
 Config.WebhookUrl = "" -- Set your Discord webhook URL here, or leave blank to disable webhook logging
 
+Config.AFKTimeoutMinutes = 30 -- Minutes before auto clock-off (set 0 to disable)
+
 Config.Departments = {
     {
         name = "LSPD",
